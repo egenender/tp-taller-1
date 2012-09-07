acba75f80393e28de27f2623e78014862f01fbef
martine9111@gmail.com <martine9111@gmail.com@19f5cbd7-13c5-56d0-b624-0bee21beae48>
2012-09-07 17:35:39 +0000

    Agregados Posicion.cpp, .h, Aplicacion.cpp y .h (nada mas modelado el Singletone). Posicion Terminada, se la podria usar como vector si se agregan los metodos getAngulo y getModulo, pero para eso hay que usar las librerias de math

---
+++ trunk/TP/src/Posicion.h
@@ -0,0 +1,41 @@
+/*
+ * Posicion.h
+ *
+ *  Created on: 06/09/2012
+ *      Author: martin
+ */
+
+#ifndef POSICION_H_
+#define POSICION_H_
+#include <stdbool.h>
+#include <stddef.h>
+
+class Posicion {
+private:
+	int x,y;
+public:
+	//Constructores y Destructores
+	Posicion();
+	Posicion(int,int);
+	virtual ~Posicion();
+
+	//Otras funciones
+	Posicion obtenerDerecha();
+	Posicion obtenerArriba();
+	Posicion obtenerAbajo();
+	Posicion obtenerIzquierda();
+	int getX();
+	int getY();
+
+	//Para verificar si una posicion esta a la X de otra
+	bool estaALaDerechaDe(Posicion);
+	bool estaALaIzquierdaDe(Posicion);
+	bool estaArribaDe(Posicion);
+	bool estaAbajoDe(Posicion);
+
+	//Para mover la posicion
+	void moverHorizontalmente(int);
+	void moverVerticalmente(int);
+};
+
+#endif /* POSICION_H_ */
