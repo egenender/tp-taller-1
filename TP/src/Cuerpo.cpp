0c46e13547992c9c8f1ce95f4f07ef5ea0580ec7
martine9111@gmail.com <martine9111@gmail.com@19f5cbd7-13c5-56d0-b624-0bee21beae48>
2012-09-08 13:27:59 +0000

    Transformado en Proyecto C/C++, agregada la clase Cuerpo (falta hacerla)

---
+++ TP/src/Cuerpo.cpp
@@ -0,0 +1,18 @@
+/*
+ * Cuerpo.cpp
+ *
+ *  Created on: 08/09/2012
+ *      Author: martin
+ */
+
+#include "Cuerpo.h"
+
+Cuerpo::Cuerpo() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Cuerpo::~Cuerpo() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ TP/src/Cuerpo.h
@@ -0,0 +1,17 @@
+/*
+ * Cuerpo.h
+ *
+ *  Created on: 08/09/2012
+ *      Author: martin
+ */
+
+#ifndef CUERPO_H_
+#define CUERPO_H_
+
+class Cuerpo {
+public:
+	Cuerpo();
+	virtual ~Cuerpo();
+};
+
+#endif /* CUERPO_H_ */
