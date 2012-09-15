14250f735f3e59bd28f3b785efbf1345491f36f0
nicodiaz55.cp@gmail.com <nicodiaz55.cp@gmail.com@19f5cbd7-13c5-56d0-b624-0bee21beae48>
2012-09-15 03:52:27 +0000

    Nico - Bueno, aca subo una nueva prueba. Camara2 es la version "trucha" de Camara :P Esta hecha para que ande con las demas cosas que hay en estado introducción, pero basicamente respeta lo que hay en la "verdadera" Camara.

---
+++ trunk/TP/src/Camara2.h
@@ -0,0 +1,23 @@
+#ifndef CAMARA2_H_
+#define CAMARA2_H_
+
+#include "Aplicacion.h"
+
+#define ANCHO_NIVEL 1200
+#define ALTO_NIVEL 622
+
+class Camara {
+	private:
+		// Las dimensiones de la camara:
+		SDL_Rect* cam;
+
+	public:
+		Camara(int x, int y);
+		void actualizar(int,int,int,int);
+		int obtenerX();
+		int obtenerY();
+		SDL_Rect* obtenerDimensiones();
+		~Camara();
+};
+
+#endif
--- trunk/TP/src/EstadoIntroduccion.cpp
+++ trunk/TP/src/EstadoIntroduccion.cpp
@@ -6,6 +6,7 @@ EstadoIntroduccion EstadoIntroduccion::instancia;
 
 // Introduccion del juego
 EstadoIntroduccion::EstadoIntroduccion() {
+	camara = NULL;
 	fondo = NULL;
 	yoshiNormal = NULL;
 	yoshiGigante = NULL;
@@ -16,9 +17,16 @@ EstadoIntroduccion::EstadoIntroduccion() {
 
 /** Inicializa el estado **/
 void EstadoIntroduccion::iniciar() {
-	fondo = new Superficie("src/fondo.jpg");
 
+	// iniciamos la camara:
+	camara = new Camara(x,y);
+
+	// cargamos el fondo:
+	fondo = new Superficie("src/fondoGrande.png");
+
+	// cargamos los cuerpos:
 	HojaSprites* normal = new HojaSprites("src/yoshi.bmp", 64, 64);
+	// color transparente: rosa
 	normal->transparencia(255, 0, 255);
 
 	HojaSprites* gigante = new HojaSprites("src/yoshi.bmp", 64, 64);
@@ -28,17 +36,24 @@ void EstadoIntroduccion::iniciar() {
 	// TODO: hacer que funcione esta linea:
 //	gigante = gigante->voltear(HORIZONTALMENTE);
 
+	// Yoshi del tamanio del original de la imagen:
 	yoshiNormal = new Animacion(normal);
+
+	// Yoshi del doble del tamanio
 	yoshiGigante = new Animacion(gigante);
 
+	// Simplemente se carga la hoja de sprites:
 	original = new Superficie("src/yoshi.bmp");
 	original->transparencia(255, 0, 255);
 
+	// Aca espejamos horizontalmente la anterior hoja:
 	espejada = original->voltear(HORIZONTALMENTE);
 }
 
 /** Termina el estado **/
 void EstadoIntroduccion::terminar() {
+
+	// Aca tenemos que eliminar lo que reservamos para este estado
 
 	delete (yoshiNormal);
 	yoshiNormal = NULL;
@@ -59,42 +74,60 @@ void EstadoIntroduccion::terminar() {
 /** Actualiza el estado **/
 void EstadoIntroduccion::actualizar() {
 
+	// Actualizamos la posicion de yoshi, tomando desde teclado:
+	// TODO: Esto se haria en una clase externa, un controlador...
 	Uint8 *keystates = SDL_GetKeyState(NULL);
 
 	if (keystates[SDLK_UP])
 		dy-=1;
-
 	if (keystates[SDLK_DOWN])
 		dy+=1;
-
 	if (keystates[SDLK_LEFT])
 		dx-=1;
-
 	if (keystates[SDLK_RIGHT])
 		dx+=1;
-
 	if (!keystates[SDLK_UP] && !keystates[SDLK_DOWN] && !keystates[SDLK_LEFT] && !keystates[SDLK_RIGHT])
 		dx = dy = 0;
 
+	// Actualizamos la posicion de los cuerpos:
 	x += dx;
 	y += dy;
 
+	// Actualizamos la posicion de la camara:
+	camara->actualizar(x,y,64,64);
+
+	// Hacemos actuar a las vistas: (si, no son vistas aun, pero esto harían las vistas)
 	yoshiNormal->animar();
 	yoshiGigante->animar();
-	// Aca se deberia cambiar a otro estado cuando se cumple una condicion
+
+
+	// Aca se deberia cambiar a otro estado cuando se cumpla una determinada condicion
+	// Por ejemplo, cuando termino el nivel, o lo que sea...
 }
 
 /** Dibuja el estado **/
 void EstadoIntroduccion::dibujar(SDL_Surface* display) {
 
-	fondo->dibujar(display, 0, 0);
+	// Obtenemos el "area" que esta mostrando la camara:
+	SDL_Rect* dimensiones = camara->obtenerDimensiones();
 
-	yoshiNormal->dibujar(display, 0, 0);
-	yoshiNormal->dibujar(display, 32, 0);
-	yoshiGigante->dibujar(display, x, y);
-	original->dibujar(display, 200, 0);
-	espejada->dibujar(display, 300, 0);
+	// Dibujamos el fondo, mostrando solo la parte que dibuja la camara:
+	fondo->dibujar(display, 0, 0, dimensiones);
 
+
+
+	/* Aca se llamaria al dibujar de todas las vistas.
+	* Guarda, tener en cuenta que se dibujan por orden, o sea,
+	* se van a solapar en el orden en que son llamados:
+	*/
+
+
+	/* Tener en cuenta que su posicion es respecto a la camara!! */
+	yoshiNormal->dibujar(display, 0 - dimensiones->x, 0 - dimensiones->y);
+	yoshiNormal->dibujar(display, 24 - dimensiones->x, 0 - dimensiones->y);
+	yoshiGigante->dibujar(display, x - dimensiones->x, y - dimensiones->y);
+	original->dibujar(display, 200 - dimensiones->x, 0 - dimensiones->y);
+	espejada->dibujar(display, 300 - dimensiones->x, 0 - dimensiones->y);
 }
 
 /** Devuelve la instancia del estado (Singleton) **/
