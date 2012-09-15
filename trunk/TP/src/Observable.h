672513fa344563b42d4d9389ac70b8fb8c9e0f8d
martine9111@gmail.com <martine9111@gmail.com@19f5cbd7-13c5-56d0-b624-0bee21beae48>
2012-09-15 21:44:18 +0000

    Modificaciones a Observable

--- trunk/TP/src/Observable.h
+++ trunk/TP/src/Observable.h
@@ -2,18 +2,21 @@
 #define OBSERVABLE_H_
 
 #include "Observador.h"
+#include <stdbool.h>
 #include <vector>
 using namespace std;
 
 class Observable {
 	private:
 		vector<Observador*> observadores;
+		bool cambios;
 	public:
 		Observable();
 		~Observable();
 		void agregarObservador(Observador* observador);
 		void eliminarObservador(Observador* observador);
 		void notificarObservadores();
+		void huboCambios();
 };
 
 #endif
