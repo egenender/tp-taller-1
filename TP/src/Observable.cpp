fe9c79c76e45ae20529a2ea5a154ffc5175e620c
nicodiaz55.cp@gmail.com <nicodiaz55.cp@gmail.com@19f5cbd7-13c5-56d0-b624-0bee21beae48>
2012-09-20 03:16:38 +0000

    Pequeño update de Observable

--- TP/src/Observable.cpp
+++ TP/src/Observable.cpp
@@ -1,24 +1,44 @@
 #include "Observable.h"
+#include <stdlib.h>
 
 Observable::Observable() {
 	cambios = false;
+	observadores = new vector<Observador*>();
 }
 
 Observable::~Observable() {
-
+	if (observadores != NULL)
+		delete(observadores);
 }
 
 void Observable::agregarObservador(Observador* observador) {
-	observadores.push_back(observador);
+	if (observador != NULL) {
+		// Verifico que ya no este agregado...
+		for (unsigned int i = 0; i < observadores->size(); i++) {
+			if (observadores->at(i) == observador)
+				return;
+		}
+
+		observadores->push_back(observador);
+
+		// Esto lo pongo porque sino, hasta que no haya un cambio en el cuerpo, no se actualiza :P
+		// Originalmente en el patron (al menos el de Java) esto aca no se hace:
+		observador->actualizar(this);
+	}
 }
 
 void Observable::eliminarObservador(Observador* observador) {
-	for (unsigned int i = 0; i < observadores.size(); i++) {
-		if (observadores[i] == observador) {
-			observadores.erase(observadores.begin() + i);
+	// Lo busco y lo elimino:
+	for (unsigned int i = 0; i < observadores->size(); i++) {
+		if (observadores->at(i) == observador) {
+			observadores->erase(observadores->begin() + i);
 			return;
 		}
 	}
+}
+
+void Observable::limpiarCambio() {
+	cambios = false;
 }
 
 void Observable::notificarObservadores() {
@@ -26,12 +46,20 @@ void Observable::notificarObservadores() {
 	if (!cambios) return;
 
 	// Se podría agregar argumentos para decirle a los observadores que fue lo que cambio:
-	for (unsigned int i = 0; i < observadores.size(); i++) {
-		observadores[i]->actualizar(this);
+	for (unsigned int i = 0; i < observadores->size(); i++) {
+		observadores->at(i)->actualizar(this);
 	}
-	cambios = false;
+	limpiarCambio();
 }
 
 void Observable::huboCambios(){
 	cambios = true;
+}
+
+bool Observable::cambio() {
+	return cambios;
+}
+
+int Observable::cantidadObservadores() {
+	return observadores->size();
 }
