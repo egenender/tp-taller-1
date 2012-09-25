5106bbd1bb1980b13f4fa5743d949edcd5fe613e
nicodiaz55.cp@gmail.com <nicodiaz55.cp@gmail.com@19f5cbd7-13c5-56d0-b624-0bee21beae48>
2012-09-25 15:43:17 +0000

    evito que se acelere por clicks de mouse... no encuentro forma de solucionar uqe toque otra tecla y se quede congelado corriendo, no se. Por alguna razon nunca se llama al if cuando pasa eso :/

--- TP/src/Controlador.cpp
+++ TP/src/Controlador.cpp
@@ -3,9 +3,9 @@
 /*Controlador* Controlador::instancia = NULL;*/
 /*
  Controlador* getInstance(){
- 	if (!instancia)
- 	 	 instancia = new Controlador();
- 	return instancia;
+ if (!instancia)
+ instancia = new Controlador();
+ return instancia;
  }
 
  * */
@@ -18,31 +18,32 @@ Controlador::~Controlador() {
 	// TODO Auto-generated destructor stub
 }
 
-void Controlador::manejarEvento(SDL_Event* evento, Manual* principal){
-	if (evento->type == SDL_MOUSEMOTION)
-				return;
+void Controlador::manejarEvento(SDL_Event* evento, Manual* principal) {
+	if (evento->type == SDL_MOUSEMOTION || evento->type == SDL_MOUSEBUTTONDOWN
+			|| evento->type == SDL_MOUSEBUTTONUP)
+		return;
 
-		Uint8 *keystates = SDL_GetKeyState(NULL);
+	Uint8 *keystates = SDL_GetKeyState(NULL);
 
-		// Podria ser mejor :P Movimiento medio choto este:
-		if (keystates[SDLK_UP]){
-			//principal->saltar(); ?
-		}
-		if (keystates[SDLK_DOWN]){
+	// Podria ser mejor :P Movimiento medio choto este:
+	if (keystates[SDLK_UP]) {
+		//principal->saltar(); ?
+	}
+	if (keystates[SDLK_DOWN]) {
 
-		}
-		if (keystates[SDLK_LEFT] && ! keystates[SDLK_RIGHT]){
-			principal->moverALaIzquierda();
-		}
+	}
+	if (keystates[SDLK_LEFT] && !keystates[SDLK_RIGHT]) {
+		principal->moverALaIzquierda();
+	}
 
-		if (keystates[SDLK_RIGHT] && !keystates[SDLK_LEFT]){
-			principal->moverALaDerecha();
-		}
+	if (keystates[SDLK_RIGHT] && !keystates[SDLK_LEFT]) {
+		principal->moverALaDerecha();
+	}
 
-		//TODO: en este if va a haber que agregar mas condiciones cuando se permita saltar/etc..
-		if (!(keystates[SDLK_LEFT] ^ keystates[SDLK_RIGHT])){
-			principal->detener();
-		}
+	//TODO: en este if va a haber que agregar mas condiciones cuando se permita saltar/etc..
+	if (!(keystates[SDLK_LEFT] ^ keystates[SDLK_RIGHT])) {
+		principal->detener();
+	}
 
 }
 
