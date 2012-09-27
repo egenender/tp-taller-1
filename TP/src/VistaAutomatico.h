20df736ef59b531073718ddf1e1cea5098eae098
martine9111@gmail.com <martine9111@gmail.com@19f5cbd7-13c5-56d0-b624-0bee21beae48>
2012-09-27 13:54:43 +0000

    Modificaciones a VistaAutomatico para poder mejorar las cosas

--- TP/src/VistaAutomatico.h
+++ TP/src/VistaAutomatico.h
@@ -11,13 +11,16 @@ using namespace std;
 
 class VistaAutomatico:public VistaAnimada {
 private:
-	int actual;
+	unsigned int actual;
 	bool terminoAhora;
 	vector<int>* periodos;
 	Timer* timer;
+
+	Animacion* animacionQuieto;
+	bool quieto;
 public:
 	VistaAutomatico(Automatico*, vector<Animacion*>*d, vector<int>*);
-	//VistaAutomatico(Automatico* , vector<Animacion*>*, vector<int>*);
+	//VistaAutomatico::VistaAutomatico(Automatico*, Animacion*, vector<Animacion*>*, vector<int>*);
 	virtual ~VistaAutomatico();
 	void actualizar(Observable* observable);
 private:
