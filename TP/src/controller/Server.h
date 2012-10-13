4096e385c0a9010324160f1aabb6f6b813a34b72
joaquinstan@gmail.com <joaquinstan@gmail.com@19f5cbd7-13c5-56d0-b624-0bee21beae48>
2012-10-13 07:36:05 +0000

    JS - Cambios conceptuales importantes sobre Server y Cliente, ademas de agregar la clase Thread para facilitar el manejo de las funciones de la lib del OS

--- TP/src/controller/Server.h
+++ TP/src/controller/Server.h
@@ -15,6 +15,8 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <map>
+#include <queue>
+using namespace std;
 
 #define PORT	5555
 typedef struct prueba{
@@ -26,33 +28,80 @@ typedef struct prueba{
 
 }prueba_t;
 
-typedef std::map <int,prueba_t> mapa_cambios;
-typedef std::map <int,int> mapa_sockid;
+//typedef std::map <int,prueba_t> mapa_cambios;
+//typedef std::map <int,int> mapa_sockid;
+
+typedef queue <void*> Cola ;
 
 class Server{
 
-
+	// CUESTION: thread para mandar tambien?
 
 	private:
+
+		// Atributos viejos
+		//mapa_cambios* cambios_entrantes;
+		//prueba_t* cambios_salientes;
+		//bool cambios_nuevos;
+
+		// No estoy seguro de la necesidad de que esten LAS DOS colas, pero prefiero mas que menos
+
+		// Esta es la cola de la que se iran desencolando los cambios que ingresen de los clientes
+		// (por ahora tipo de dato void* hasta decision)
+		Cola cola_entrantes;
+		// Mismo, pero los cambios que deben salir a los clientes
+		Cola cola_salientes;
+
+		// Crea un socket a partir de un puerto en el servidor
 		int crear_socket (unsigned short int);
+
+		// Atributo puerto del servidor
 		int puerto;
+		// Esta activado o no
 		bool activado;
+		// Conjuntos de sockets (veremos esto)
 		fd_set activos,leidos;
+		// El file descriptor del socket del servidor
 		int sock;
-		mapa_cambios* cambios_entrantes;
-		prueba_t* cambios_salientes;
-		bool cambios_nuevos;
 
 	public:
+
+		// Metodos viejos
+		//void atender_sockets(void);
+		//void informar_cambios();
+		//bool leer_de_cliente (int, void*,size_t);
+		//bool escribir_a_cliente(int,void*,size_t);
+		//void iniciar ();
+		//void activar();
+		//void desactivar();
+
+		// Crea un server asi noma
 		Server();
+
+		// Crea un server especificando un Port
 		Server(int);
-		void informar_cambios();
-		void atender_sockets(void);
-		bool leer_de_cliente (int, void*,size_t);
-		bool escribir_a_cliente(int,void*,size_t);
-		void iniciar ();
-		void activar();
-		void desactivar();
+
+		// Esto deberia crear un thread que empiece a escuchar a los clientes que le piden conexion,
+		// aceptarlos y encolar sus cambios entrantes
+		int escuchar();
+
+		// Esto deberia desencolar de las cosas que le mandan para escribir y mandarlo a todos los clientes
+		int escribir_cambio();
+
+		// Encola un cambio para recibir (servidor: recibir cambio)
+		int recibir_cambio(void*);
+
+		// Encola un cambio para mandar (enviar cambio a servidor)
+		int enviar_cambio(void*);
+
+		// Devuelve el cambio mas actual (de la cola) para aplicar en el modelo
+		void* devolver_cambio();
+
+		// Detener al server
+		int detener ();
+
+
+
 
 
 };
