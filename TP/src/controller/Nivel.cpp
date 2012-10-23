d9268b7b2c9e932c06198044acf3509fd6e52b97
egenender@gmail.com <egenender@gmail.com@19f5cbd7-13c5-56d0-b624-0bee21beae48>
2012-10-23 04:02:13 +0000

    atiende el Nivel-> parar

--- TP/src/controller/Nivel.cpp
+++ TP/src/controller/Nivel.cpp
@@ -95,14 +95,14 @@ void Nivel::terminar() {
 }
 
 void Nivel::actualizar(float delta) {
-//	if (parar && estado==CLIENTE){
-//		Cliente::obtenerInstancia("",0)->detener_escuchar();
-//		Cliente::obtenerInstancia("",0)->detener_escribir();
-//		Cliente::obtenerInstancia("",0)->detener();
-//		GestorConfiguraciones::getInstance()->acabarGestor();
-//		ManejadorEstados::setearEstadoActual(ESTADO_GUI);
-//		return;
-//	}
+	if (parar && estado==CLIENTE){
+		Cliente::obtenerInstancia("",0)->detener_escuchar();
+		Cliente::obtenerInstancia("",0)->detener_escribir();
+		Cliente::obtenerInstancia("",0)->detener();
+		GestorConfiguraciones::getInstance()->acabarGestor();
+		ManejadorEstados::setearEstadoActual(ESTADO_GUI);
+		return;
+	}
 	// Actualizamos cada cuerpo:
 	for (unsigned int i = 0; i < actualizables->size(); i++) {
 		Actualizable* cuerpito = actualizables->at(i);
