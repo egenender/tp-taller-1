162974b938194d0bd85c33ce24998179288f38c9
martine9111@gmail.com <martine9111@gmail.com@19f5cbd7-13c5-56d0-b624-0bee21beae48>
2012-10-24 02:14:37 +0000

    ControladorCliente mejorado para mejorar fluides

--- TP/src/controller/ControladorCliente.h
+++ TP/src/controller/ControladorCliente.h
@@ -7,14 +7,15 @@
 class ControladorCliente {
 private:
 	unsigned int ID;
-	int ultimoEstado;
+	//int ultimoEstado;
+	bool saltando, izquierda, derecha, quieto;
 public:
 	ControladorCliente(unsigned int id);
 	virtual ~ControladorCliente();
 	void manejarEvento(SDL_Event*);
 private:
 	void enviarStruct(int);
-	void revisarCambio(int);
+	//void revisarCambio(int);
 };
 
 #endif /* CONTROLADORCLIENTE_H_ */
