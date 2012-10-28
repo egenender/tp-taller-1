bc67f0def0ad1965ebda9b327593cde4f0c9f4a1
nicodiaz55.cp@gmail.com <nicodiaz55.cp@gmail.com@19f5cbd7-13c5-56d0-b624-0bee21beae48>
2012-10-28 03:46:34 +0000

    Primer clase para sonidos... si alguien tiene ganas de editar un poco el gestor, lo podemos probar.

---
+++ TP/src/view/VistaSonora.cpp
@@ -0,0 +1,75 @@
+#include "VistaSonora.h"
+#include "Log.h"
+
+void VistaSonora::VistaSonora() {
+	sonidoActual = NULL;
+	sonidos = new map<int,Mix_Chunk*>();
+	pararDeReproducir = false;
+	debeReproducir = false;
+}
+
+Mix_Chunk* VistaSonora::cargarSonido(string ruta) {
+	return Mix_LoadWAV(ruta.c_str());
+}
+
+bool VistaSonora::agregarSonido(string ruta, int estado) {
+	if (!sonidos)
+		return false;
+
+	Mix_Chunk* sonido = cargarSonido(ruta);
+	if (!sonido)
+		return false;
+
+	sonidos->insert(pair<int, Mix_Chunk*>(estado, sonido));
+
+	return true;
+}
+
+VistaSonora::~VistaSonora() {
+	if (sonidos != NULL) {
+		map<int, Mix_Chunk*>::iterator iter;
+		for (iter = sonidos->begin(); iter != sonidos->end(); ++iter) {
+			Mix_Chunk* sound = (*iter).second;
+			if (sound != NULL) {
+				Mix_FreeChunk(sound);
+				sound = NULL;
+			}
+		}
+		sonidos->clear();
+		delete(sonidos);
+		sonidos = NULL;
+	}
+
+	debeReproducir = false;
+	pararDeReproducir = true;
+}
+
+void VistaSonora::actualizar(Observable* observable) {
+	int estado = observable->obtenerEstado();
+	if (estado == MUERTO){
+		pararDeReproducir = true;
+		debeReproducir = false;
+		return;
+	}
+	pararDeReproducir = false;
+	debeReproducir = true;
+
+	sonidoActual = sonidos->at(estado);
+}
+
+bool VistaSonora::reproducir() {
+	if (sonidoActual == NULL) {
+		Log::getInstance()->writeToLogFile(Log::ADVERTENCIA, "Sonido Actual es NULL");
+		return false;
+	}
+
+	if (debeReproducir && !pararDeReproducir) {
+		 if(Mix_PlayChannel(-1, sonidoActual, 0) == -1) {
+			 Log::getInstance()->writeToLogFile(Log::ERROR, "Error al intentar reproducir sonido actual");
+			 return false;
+		 }
+		 debeReproducir = false;
+	}
+
+	return false;
+}
---
+++ TP/src/view/VistaSonora.h
@@ -0,0 +1,34 @@
+/*
+ * VistaSonora.h
+ *
+ *  Created on: 27/10/2012
+ *      Author: nicolas
+ */
+
+#ifndef VISTASONORA_H_
+#define VISTASONORA_H_
+
+#include "SDL/SDL.h"
+#include "SDL/SDL_mixer.h"
+#include "../model/Observable.h"
+#include <map>
+using namespace std;
+
+class VistaSonora {
+protected:
+	map<int,Mix_Chunk*>* sonidos;
+	Mix_Chunk* sonidoActual;
+	bool pararDeReproducir, debeReproducir;
+private:
+	void inicializar();
+	Mix_Chunk* cargarSonido(string ruta);
+public:
+	VistaSonora();
+	~VistaSonora();
+
+	bool agregarSonido(string ruta, int estado);
+	void actualizar(Observable* observable);
+	bool reproducir(); // aka: dibujar();
+};
+
+#endif /* VISTASONORA_H_ */
