652b660d1a2dac108904710225e71294cebcc65d
nicodiaz55.cp@gmail.com <nicodiaz55.cp@gmail.com@19f5cbd7-13c5-56d0-b624-0bee21beae48>
2012-10-29 01:23:15 +0000

    ordeno un poco esto.

--- TP/src/view/VistaProtagonista.cpp
+++ TP/src/view/VistaProtagonista.cpp
@@ -6,23 +6,21 @@ VistaProtagonista::VistaProtagonista(/*Observable* protagonista,*/ Animacion* ca
 	//primeraVez = true;
 
 	Animacion* caminaDer = caminar;
-	caminaDer->transparencia(255,0,255); // color transparente = magenta
-
+	caminaDer->transparencia(255,0,255);
 	Animacion* caminaIzq = caminaDer->voltear(HORIZONTALMENTE);
 
 	Animacion* quietoDer=quieto;
 	quietoDer->transparencia(255,0,255);
-
 	Animacion* quietoIzq = quietoDer->voltear(HORIZONTALMENTE);
+
+	Animacion* saltandoDer = saltar;
+	saltandoDer->transparencia(255,0,255);
+	Animacion* saltandoIzq = saltandoDer->voltear(HORIZONTALMENTE);
 
 	animaciones->insert(pair<int, Animacion*>(QUIETODER, quietoDer));
 	animaciones->insert(pair<int, Animacion*>(QUIETOIZQ, quietoIzq));
 	animaciones->insert(pair<int, Animacion*>(CAMINANDODER,caminaDer));
 	animaciones->insert(pair<int, Animacion*>(CAMINANDOIZQ,caminaIzq));
-
-	Animacion* saltandoDer = saltar;
-	saltandoDer->transparencia(255,0,255);
-	Animacion* saltandoIzq = saltandoDer->voltear(HORIZONTALMENTE);
 	animaciones->insert(pair<int, Animacion*>(SALTANDODER, saltandoDer));
 	animaciones->insert(pair<int, Animacion*>(SALTANDOIZQ, saltandoIzq));
 
