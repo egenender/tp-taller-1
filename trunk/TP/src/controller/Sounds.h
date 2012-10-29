d3605f7357b6c95ead2c2753a159dee04a04a610
cordobacesar@gmail.com <cordobacesar@gmail.com@19f5cbd7-13c5-56d0-b624-0bee21beae48>
2012-10-29 00:11:50 +0000

    Sounds path

---
+++ trunk/TP/src/controller/Sounds.h
@@ -0,0 +1,73 @@
+/*
+ * Sounds.h
+ * Defino las constantes para el acceso a los archivos .WAV
+ *
+ *  Created on: 28/10/2012
+ *      Author: loco_cesar
+ */
+
+#ifndef SOUNDS_H_
+#define SOUNDS_H_
+
+#define ONE_UP "../sounds/smb3_1-up.wav"
+#define AIR_SHIP_CLEAR "../sounds/smb3_airship_clear.wav"
+#define AIR_SHIP_MOVES "../sounds/smb3_airship_moves.wav"
+#define BONUS_GAME_NO_MATCH "../sounds/smb3_bonus_game_no_match.wav"
+#define BOWSER_FALLS "../sounds/smb3_bowser_falls.wav"
+#define BREACK_BRICK_BLOCK "../sounds/smb3_break_brick_block.wav"
+#define BUMP "../sounds/smb3_bump.wav"
+#define COIN "../sounds/smb3_coin.wav"
+#define ENTER_LEVEL "../sounds/smb3_enter_level.wav"
+#define FIREBALL "../sounds/smb3_fireball.wav"
+#define FORTRESS "../sounds/smb3_fortress_clear.wav"
+#define FROG_MARIO_WALK "../sounds/smb3_frog_mario_walk.wav"
+#define GAME_OVER "../sounds/smb3_game_over.wav"
+#define HAMMER_BROS_SHUFFLE "../sounds/smb3_hammer_bros_shuffle.wav"
+#define HURRY_UP "../sounds/smb3_hurry_up.wav"
+#define INVENTORY_OPEN_CLOSE "../sounds/smb3_inventory_open_close.wav"
+#define JUMP "../sounds/smb3_jump.wav"
+#define JUMP_SMALL "../sounds/smb3_jump-small.wav"
+#define KICK "../sounds/smb3_kick.wav"
+#define LEVEL_CLEAR "../sounds/smb3_level_clear.wav"
+#define LOST_SUIT "../sounds/smb3_lost_suit.wav"
+#define MAP_NEW_WORLD "../sounds/smb3_map_new_world.wav"
+#define MAP_TRAVEL "../sounds/smb3_map_travel.wav"
+#define MUSHROOM_APPEARS "../sounds/smb3_mushroom_appears.wav"
+#define NSPADE_MATCH "../sounds/smb3_nspade_match.wav"
+#define PAUSE "../sounds/smb3_pause.wav"
+#define PIPE "../sounds/smb3_pipe.wav"
+#define PLAYER_DOWN "../sounds/smb3_player_down.wav"
+#define PMETER "../sounds/smb3_pmeter.wav"
+#define POWER_UP "../sounds/smb3_power-up.wav"
+#define RACCON_TRANSFORM "../sounds/smb3_raccoon_transform.wav"
+#define STOMP "../sounds/smb3_stomp.wav"
+#define TAIL "../sounds/smb3_tail.wav"
+#define TEXT "../sounds/smb3_text.wav"
+#define THWOMP "../sounds/smb3_thwomp.wav"
+#define VINE "../sounds/smb3_vine.wav"
+#define WISTLE "../sounds/smb3_whistle.wav"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /* SOUNDS_H_ */
