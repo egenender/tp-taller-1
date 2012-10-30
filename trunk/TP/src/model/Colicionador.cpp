7fda4ff34d14a026e1bac6b83940a3dec785f97c
martine9111@gmail.com <martine9111@gmail.com@19f5cbd7-13c5-56d0-b624-0bee21beae48>
2012-10-30 01:48:48 +0000

    El nivel verificando colisiones

--- trunk/TP/src/model/Colicionador.cpp
+++ trunk/TP/src/model/Colicionador.cpp
@@ -1,5 +1,7 @@
 #include "Colisionador.h"
 #include <stdbool.h>
+#include <stdio.h>
+#include "Cuerpo.h"
 using namespace std;
 
 bool colisionan(Actualizable*, Actualizable*);
@@ -11,11 +13,10 @@ void generarColisiones (vector<Actualizable*>* actualizables){
 	for (unsigned int i = 0; i < actualizables->size(); i++){
 		primero = actualizables->at(i);
 		for (unsigned int j = 0; j < actualizables->size(); j++){
-			if (j != i){
-				segundo = actualizables->at(j);
-				if (colisionan(primero, segundo))
-					colisionar(primero,segundo);
-			}
+			if (i == j) continue;
+			segundo = actualizables->at(j);
+			if (colisionan(primero, segundo))
+				colisionar(primero,segundo);
 		}
 	}
 
@@ -26,11 +27,15 @@ bool colisionan(Actualizable* primero, Actualizable* segundo){
 	Area *area1 = primero->obtenerArea();
 	Area *area2 = segundo->obtenerArea();
 
-	bool huboColision = (area1->colisionaConOtra(area2) || area2->colisionaConOtra(area2));
+	if (area1 == NULL) return false;
+	if (area2 == NULL) return false;
+
+	bool huboColision = (area1->colisionaConOtra(area2) || area2->colisionaConOtra(area1));
 	return huboColision;
 }
 
 void colisionar(Actualizable* primero, Actualizable* segundo){
 	//TODO
 	//Para este commmit no hago nada
+	printf("HUBO COLISION\n");
 }
