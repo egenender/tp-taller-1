347296537292ca88bbef6d88edea15f3bed83305
martine9111@gmail.com <martine9111@gmail.com@19f5cbd7-13c5-56d0-b624-0bee21beae48>
2012-11-02 15:53:28 +0000

    Solucionados los problemas con las Plataformas ;D

--- TP/src/model/Manual.cpp
+++ TP/src/model/Manual.cpp
@@ -46,13 +46,15 @@ Manual::Manual(const char* nombrecito, Area* sup, int vel, int fuerza):Cuerpo(no
 	chocaConEscalera = false;
 	posAnterior = NULL;
 
-//	int ancho = sup->obtenerAncho();
-//	int alto = sup->obtenerAlto();
-//
-//	int anchoC = ancho * 3 / 4;
-//	int altoC = alto * 3 / 4;
-//	Posicion* posC = new Posicion(sup->obtenerPosicion()->obtenerX() + (ancho/8), sup->obtenerPosicion()->obtenerY() + (alto/8));
-//	superficieDeColision = new Area(anchoC, altoC, posC);
+	int ancho = sup->obtenerAncho();
+	int alto = sup->obtenerAlto();
+
+	int anchoC = (ancho * FACTOR_DE_ESCALA )/100;
+	int altoC = (alto * FACTOR_DE_ESCALA) / 100;
+	int difAncho = ancho - anchoC;
+	int difAlto = alto - altoC;
+	Posicion* posC = new Posicion(sup->obtenerPosicion()->obtenerX() + (difAncho/2), sup->obtenerPosicion()->obtenerY() + (difAlto));
+	superficieDeColision = new Area(anchoC, altoC, posC);
 
 }
 
@@ -69,18 +71,18 @@ void Manual::movimiento(int saltando, int caminando, int direccion){
 		estado = saltando;
 	else
 		estado = caminando;
-	trasladar(direccion * velocidad, 0);
+	trasladar(direccion * velocidad, 0,true);
 }
 
 
-void Manual::trasladar(int factorX, int factorY){
-	//float DeltaX = factorX * delta;
-	//float DeltaY = factorY * delta;
-	if (posAnterior) delete(posAnterior);
-	posAnterior = new Posicion(obtenerArea()->obtenerPosicion()->obtenerX(),obtenerArea()->obtenerPosicion()->obtenerY());
+void Manual::trasladar(int factorX, int factorY, bool cambio){
+	if (cambio){
+		if (posAnterior) delete(posAnterior);
+		posAnterior = new Posicion(obtenerArea()->obtenerPosicion()->obtenerX(),obtenerArea()->obtenerPosicion()->obtenerY());
+	}
 	Posicion* posDesplazamiento = new Posicion (factorX,factorY);
 	superficieOcupada->mover(posDesplazamiento);
-//	superficieDeColision->mover(posDesplazamiento);
+	superficieDeColision->mover(posDesplazamiento);
 
 	delete(posDesplazamiento);
 	huboCambios();
@@ -125,10 +127,14 @@ void Manual::detener(){
 void Manual::actualizarSalto(){
 	if (!estoySaltando()) return;
 
-	trasladar(0,velocidadY);
+	trasladar(0,velocidadY,true);
 	velocidadY += ACELERACION;
 	if (chocaConPiso() || tengoPiso){
+		superficieDeColision->ponerEnPiso();
 		superficieOcupada->ponerEnPiso();
+//		//Posicion* trs = new Posicion (0, (superficieOcupada->obtenerAlto()-superficieDeColision->obtenerAlto()));
+//		superficieOcupada->mover(trs);
+//		delete(trs);
 		velocidadY = 0;
 		if (estado == SALTANDODER) estado = QUIETODER;
 		else estado = QUIETOIZQ;
@@ -136,7 +142,7 @@ void Manual::actualizarSalto(){
 }
 
 bool Manual::chocaConPiso(){
-	return superficieOcupada->pasaPiso();
+	return obtenerArea()->pasaPiso();
 }
 
 void Manual::morir(){
@@ -148,7 +154,7 @@ bool Manual::estaMuerto(){
 }
 
 void Manual::validarPiso(){
-	if (superficieOcupada->estaSobreElPiso()){
+	if (obtenerArea()->estaSobreElPiso()){
 		tengoPiso = true;
 		return;
 	}
@@ -167,14 +173,14 @@ void Manual::subir(){
 
 	puedoSubir = false;
 	//por ahora digo que la velocidad a la que sube, es la misma a la que se mueve
-	trasladar(0, -velocidad);
+	trasladar(0, -velocidad,true);
 	estado = SUBIENDOMOVIMIENTO;
 }
 
 void Manual::bajar(){
 	if (!puedoSubir) return;
 	puedoSubir = false;
-	trasladar(0,velocidad);
+	trasladar(0,velocidad,true);
 	estado = SUBIENDOMOVIMIENTO;
 }
 
@@ -199,19 +205,22 @@ void Manual::chocarConPlataforma(Plataforma* p){
 			delete(cmpAbajo);
 			int mov = p->obtenerArea()->obtenerPosicion()->obtenerY() + p->obtenerArea()->obtenerAlto();
 			mov -= obtenerArea()->obtenerPosicion()->obtenerY();
-			trasladar(0,mov+1);
+			trasladar(0,mov+1,true);
 			velocidadY = 0;
 			return;
 		}
 		delete(cmpAbajo);
 
 		Posicion* cmpIzquierda = new Posicion(posAnterior->obtenerX() + obtenerArea()->obtenerAncho(), posAnterior->obtenerY());
-		if (cmpIzquierda->estaALaIzquierdaDe(p->obtenerArea()->obtenerPosicion())){
+		printf("La posicion en X (cmp) es: %d ",cmpIzquierda->obtenerX());
+		printf("La posicion en X del bloque es: %d\n",p->obtenerArea()->obtenerPosicion()->obtenerX());
 
+		if (cmpIzquierda->estaALaIzquierdaOIgualDe(p->obtenerArea()->obtenerPosicion())){
+			printf("Entre\n");
 			delete(cmpIzquierda);
 			int mov = obtenerArea()->obtenerPosicion()->obtenerX() + obtenerArea()->obtenerAncho();
 			mov	-= p->obtenerArea()->obtenerPosicion()->obtenerX();
-			trasladar(-mov-1,0);
+			trasladar(-mov-1,0,true);
 			return;
 		}
 
@@ -221,23 +230,29 @@ void Manual::chocarConPlataforma(Plataforma* p){
 			delete (cmpDer);
 			int x = p->obtenerArea()->obtenerPosicion()->obtenerX() +p->obtenerArea()->obtenerAncho();
 			x -= obtenerArea()->obtenerPosicion()->obtenerX();
-			trasladar(x+1,0);
+			trasladar(x+1,0,true);
 			return;
 		}
 		delete(cmpDer);
 //		tengoPiso = true;
 //		return;
 	}
+	bool cambio = true;
 	if(posCmp)
 		delete(posCmp);
+	else{
+		cambio = false;
+	}
+
+	tengoPiso = true;
+	chocaConSosten = true;
 
 	int y;
 	y = obtenerArea()->obtenerPosicion()->obtenerY() + obtenerArea()->obtenerAlto();
 	y -= p->obtenerArea()->obtenerPosicion()->obtenerY();
 
-	trasladar(0,-y);
-	tengoPiso = true;
-	chocaConSosten = true;
+	trasladar(0,-y,cambio);
+
 }
 void Manual::chocarConEscalera(Escalera*){
 	if (!estoySubiendo()) estado = SUBIENDOQUIETO;
