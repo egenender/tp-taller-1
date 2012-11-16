30414ce77126e3bc5ecd60fdcd6634d4b9e80bc8
martine9111@gmail.com <martine9111@gmail.com@19f5cbd7-13c5-56d0-b624-0bee21beae48>
2012-11-16 23:58:54 +0000

    Bien las vigas (imagen)

--- trunk/TP/src/view/VistaImagen.cpp
+++ trunk/TP/src/view/VistaImagen.cpp
@@ -1,4 +1,6 @@
 #include "VistaImagen.h"
+#include <math.h>
+#include "../model/Cuerpo.h"
 
 VistaImagen::VistaImagen() {
 	imagen = NULL;
@@ -6,6 +8,12 @@ VistaImagen::VistaImagen() {
 
 VistaImagen::VistaImagen(Superficie* imagenNueva, int angulo) {
 	imagen = imagenNueva;
+	if (angulo < 0)
+		angulo += 2;
+
+	if (angulo > 0)
+		angulo -= 2;
+	alpha = angulo;
 	if (imagen) {
 		imagen->girar(angulo);
 	}
@@ -34,11 +42,18 @@ void VistaImagen::cambiarImagen(Superficie* imagenNueva) {
 
 void VistaImagen::actualizar(Observable* observable) {
 	posicionDibujar = observable->obtenerPosicion();
+	movY = 0;
+	Cuerpo* c = (Cuerpo*)observable;
+	if (alpha > 0){
+		float angulito = alpha * 3.1415 / 180;
+		movY = c->obtenerAncho() * sin (angulito) ;
+	}
+
 }
 
 bool VistaImagen::dibujar(SDL_Surface* display, int xCamara, int yCamara) {
 	if (display == NULL || imagen == NULL || posicionDibujar == NULL)
 		return false;
 
-	return imagen->dibujar(display, posicionDibujar->obtenerX() - xCamara, posicionDibujar->obtenerY() - yCamara);
+	return imagen->dibujar(display, posicionDibujar->obtenerX() - xCamara, posicionDibujar->obtenerY() - yCamara - movY);
 }
