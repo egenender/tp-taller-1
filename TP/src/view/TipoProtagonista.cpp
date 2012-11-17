0dafe2b16125fa911c1e100e53ade5440409924c
egenender@gmail.com <egenender@gmail.com@19f5cbd7-13c5-56d0-b624-0bee21beae48>
2012-11-17 16:33:39 +0000

    tipoProt soportando todas las animaciones

--- TP/src/view/TipoProtagonista.cpp
+++ TP/src/view/TipoProtagonista.cpp
@@ -16,6 +16,15 @@ TipoProtagonista::TipoProtagonista (){
 	animacionPasivaProt=0;
 	animacionActivaProt=0;
 	animacionSaltaProt=0;
+	animacionEscaleraProt=0;
+	animacionEscalarProt=0;
+	animacionHeridoProt=0;
+	animacionPasivaProt=0;
+	animacionActivaEvo=0;
+	animacionSaltaEvo=0;
+	animacionEscaleraEvo=0;
+	animacionEscalarEvo=0;
+	animacionHeridoEvo=0;
 	salto = 0;
 	vistaSonora=0;
 	disponible = true;
